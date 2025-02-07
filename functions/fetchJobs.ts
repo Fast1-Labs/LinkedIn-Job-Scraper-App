2ed6203e64282d6a3892b2a043349437f213637e
anilyavas <anil.yavas99@gmail.com>
2025-02-07 15:22:50 +0300

    Refactor fetchJobs function to use axios instead of fetch

--- functions/fetchJobs.ts
+++ functions/fetchJobs.ts
@@ -1,22 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = process.env.EXPO_PUBLIC_BRIGHTDATA_API_KEY;
+const DATASET_ID = 'gd_lpfll7v5hcqtkxl6l';
+const URL = `https://api.brightdata.com/datasets/v3/trigger?dataset_id=${DATASET_ID}&include_errors=true&type=discover_new&discover_by=keyword`;
+
 export const fetchJobs = async (keyword: string) => {
   try {
-    const response = await fetch(
-      `https://brightdata.com/cp/data_api/gd_lpfll7v5hcqtkxl6l/keyword?id=hl_c6db08c4&keyword=${keyword}`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${process.env.EXPO_PUBLIC_BRIGHTDATA_API_KEY}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-
-    const text = await response.text(); // Get the response as text
-    console.log('Response:', text); // Log the response
-    const data = JSON.parse(text); // Parse the text as JSON
-    return data;
+    const response = await axios.get(URL, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    return response.data.data || [];
   } catch (error) {
-    console.error('Error while fetching jobs! ', error);
-    return null;
+    console.error('Error while fetching jobs ', error);
+    return [];
   }
 };
